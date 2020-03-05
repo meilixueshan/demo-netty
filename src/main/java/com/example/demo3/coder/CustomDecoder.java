9463bdb5f95ac74096b5cb9739a8c076913bc478
xw <xuwei@hanboard.com>
2020-03-05 09:38:17 +0800

    comment: 增加注释

--- src/main/java/com/example/demo3/coder/CustomDecoder.java
+++ src/main/java/com/example/demo3/coder/CustomDecoder.java
@@ -11,29 +11,42 @@ import java.util.List;
 public class CustomDecoder extends ByteToMessageDecoder {
     @Override
     protected void decode(ChannelHandlerContext ctx, ByteBuf byteBuf, List<Object> out) throws Exception {
-        //读取消息头,整个消息的长度字段
+        //读取消息头,整个消息的长度字段。可读字节小于4，说明消息长度还没满，直接返回
         if (byteBuf.readableBytes() < 4) {
             return;
         }
 
+        // 设置回滚点
         byteBuf.markReaderIndex();
-        int dataLength = byteBuf.readInt();//读取一个规定的int，即长度
-        if (dataLength < 0) {
+
+        // 读取前四个字节(消息头)存储的消息长度，同时会使readerIndex向前移动4个指针
+        int dataLength = byteBuf.readInt();
+
+        // 读到的消息体长度为0，这是不应该出现的情况，这里出现这情况，关闭连接。
+        if (dataLength <= 0) {
             ctx.close();
         }
 
-        //读取字节数组,直到读取的字节数组长度等于dataLength
+        // 如果可读字节数小于消息长度，说明消息还不完整。
         if (byteBuf.readableBytes() < dataLength) {
+            // 重置读指针，并返回
             byteBuf.resetReaderIndex();
             return;
         }
 
         byte[] data = new byte[dataLength];
-        byteBuf.readBytes(data);
+        // 将ByteBuf中的数据读到字节数组中
+        byteBuf.readBytes(data); // 与byteBuf.readBytes(data, 0, dataLength)等价
 
-        //将字节数组使用Hession反序列化为对象
+        //将字节数组反序列化为对象
         CustomMsg customMsg = SerializerUtils.deserialize(data, CustomMsg.class);
 
         out.add(customMsg);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
     }
 }
